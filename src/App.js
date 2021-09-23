eb8fc6794b7ad1fa01e36c8464831d2624f7cc9a
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-23 17:28:14 +0530

    connected backend & frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ChatEngine } from 'react-chat-engine'
+
 
 const App = () => {
     return (
-        <div>
-           app comp 
-        </div>
+        <ChatEngine
+        projectID='990fd885-033b-47f6-b2cd-31a69cce97a4'
+        userName='Chaitanya'
+        userSecret='Chaitanya'
+      />
     )
 }
 
