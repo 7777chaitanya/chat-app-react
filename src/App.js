7e75219c2d9ead74ce85503dc796bcafb0bca2ea
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-23 17:41:00 +0530

    firebase setup done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { ChatEngine } from 'react-chat-engine'
 
 
 const App = () => {
     return (
-        <ChatEngine
-        projectID='990fd885-033b-47f6-b2cd-31a69cce97a4'
-        userName='Chaitanya'
-        userSecret='Chaitanya'
-      />
+    
     )
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7t7VxVxaUrQVIsdAhvg7PtYTOZ3gW4PU",
+  authDomain: "chat-application-88c1a.firebaseapp.com",
+  projectId: "chat-application-88c1a",
+  storageBucket: "chat-application-88c1a.appspot.com",
+  messagingSenderId: "556000263145",
+  appId: "1:556000263145:web:b01fb8112ea92bea4e2978"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
