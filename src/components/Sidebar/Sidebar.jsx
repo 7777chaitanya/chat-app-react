42e0313e4c2a0f90a4a8faab24290e7236bbc9b9
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-23 20:01:56 +0530

    created sidebar searchbar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,7 @@ import useStyles from "./styles";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 // import SidebarHeader from "./SidebarHeader";
 // import SidebarSearch from "./SidebarSearch";
 // import SidebarChats from "./SidebarChats"
@@ -15,13 +16,22 @@ const Sidebar = () => {
       <div className={classes.sidebar__header}>
         <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         <div className={classes.sidebar__header__icons}>
-          <DonutLargeIcon className={classes.sidebar__header__icon}/>
-          <ChatIcon className={classes.sidebar__header__icon}/>
+          <DonutLargeIcon className={classes.sidebar__header__icon} />
+          <ChatIcon className={classes.sidebar__header__icon} />
           <MoreVertIcon />
         </div>
       </div>
-      <div className="sidebar_search"></div>
-      <div className="sidebar__chats"></div>
+      <div className={classes.sidebar_search__container}>
+      <div className={classes.sidebar_search}>
+
+        <div className={classes.sidebar_searchicon_div}>
+        <SearchIcon className={classes.searchIcon}/>
+        </div>
+        <input type="text" placeholder="search your friend here" className={classes.inputField}/>
+      </div>
+      </div>
+
+      <div className={classes.sidebar__chats}></div>
     </div>
   );
 };
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -1,20 +1,54 @@
 import { makeStyles } from "@material-ui/core";
 
-const  useStyles = makeStyles({
-    sidebar : {
-        flex : 3,
-        borderRight : "1px solid black"
-    },
-    sidebar__header : {
-        display : "flex",
-        justifyContent : "space-between",
-        padding : "1rem",
-        alignItems : 'center',
-        backgroundColor : "rgb(235,235,235)"
-    },
-    sidebar__header__icon : {
-            marginRight : "2rem"
-    }
-})
+const useStyles = makeStyles({
+  sidebar: {
+    flex: 3,
+    borderRight: "1px solid black",
+  },
+  sidebar__header: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "1rem",
+    alignItems: "center",
+    backgroundColor: "rgb(235,235,235)",
+    border: "1px solid black",
+  },
+  sidebar__header__icon: {
+    marginRight: "2rem",
+  },
+  searchIcon: {
+    margin: "auto 1rem",
+    color: "darkgray",
+    backgorundColor: "white",
+    marginTop : "0.5rem"
+    
+
+  },
+  sidebar_search: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    border: "1rem solid white",
+    height : "1rem",
+    backgroundColor : "white",
+    borderRadius : "50px",
+  },
+  inputField: {
+    width: "100%",
+    border: "none",
+    height : "2rem",
+    "&:focus": {
+       outlineWidth : "0rem"
+      }
+  },
+  sidebar_searchicon_div : {
+      backgroundColor : "white"
+  },
+  sidebar_search__container : {
+      padding: "0.5rem",
+      backgroundColor : "rgb(235,235,235)",
+
+  }
+});
 
 export default useStyles;
