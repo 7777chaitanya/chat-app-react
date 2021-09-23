320fc23ee5de3256a5c1af0bec98b54e621fc1bd
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-23 18:05:45 +0530

    material ui installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import "./App.css"
 
 const App = () => {
     return (
-    
+        <div className="app">
+            <div className="app__body">
+                {/* sidebar with people list */}
+hi
+                {/* messages */}
+            </div>
+        </div>
     )
 }
 
