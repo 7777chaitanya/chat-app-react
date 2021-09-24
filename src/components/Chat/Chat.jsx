17112b860f4535975e7341d869ad2e4bc0b42699
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-24 10:20:36 +0530

    implemented the sidebar component

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,18 +1,82 @@
-import React from "react";
+import { IconButton, CardHeader, Avatar } from "@material-ui/core";
+import React, {useRef, useState} from "react";
 import useStyles from "./styles";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+import SentimentVerySatisfiedIcon from "@material-ui/icons/SentimentVerySatisfied";
+import AttachmentIcon from "@material-ui/icons/Attachment";
 
 const Chat = () => {
   const classes = useStyles();
+  const [message, setMessage] = useState("");
+  const messageRef = useRef();
 
- 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(messageRef.current.value);
+    messageRef.current.value="";
+  }
 
   return (
     <div className={classes.chat}>
-      <div className="chat__header">header</div>
+      <div className={classes.chat__header}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <>
+              {/* <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton> */}
+              <IconButton aria-label="settings">
+                <SearchIcon />
+              </IconButton>
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            </>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+      </div>
 
-      <div className="chat__body">body</div>
+      <div className={classes.chat__body}>
+        <div className={classes.chat__message}>
+          <span className={classes.chat__user}>Chaitanya</span>
+          hello
+          <span className={classes.chat__timestamp}>03:52</span>
+        </div>
+        <div className={classes.chat__message}>
+          <span className={classes.chat__user}>Chaitanya</span>
+          wassup guys
+          <span className={classes.chat__timestamp}>03:52</span>
+        </div>
+        <div className={classes.chat__receiver}>
+          <span className={classes.chat__user}>Chaitanya</span>
+          helllllllo
+          <span className={classes.chat__timestamp}>03:52</span>
+        </div>
+      </div>
 
-      <div className="chat__footer">footer</div>
+      <div className={classes.chat__footer}>
+        <SentimentVerySatisfiedIcon className={classes.footerIcons} />
+        <form action="submit" className={classes.chat__footer__messageform} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter your message here!"
+            className={classes.messageInput}
+            ref={messageRef}
+          />
+          <button type="submit" className={classes.submitButton}>
+            submit
+          </button>
+        </form>
+        <AttachmentIcon className={classes.footerIcons} />
+      </div>
     </div>
   );
 };
--- src/components/Chat/styles.js
+++ src/components/Chat/styles.js
@@ -1,15 +1,99 @@
 import { createTheme, makeStyles } from "@material-ui/core";
+import { red } from '@material-ui/core/colors';
+import image from "./whatsappbackground.png";
 
-
-
-const  useStyles = makeStyles(theme =>({
-chat : {
-    flex : 7,
-    [theme.breakpoints.down('sm')]: {
-        display : "none"
+const useStyles = makeStyles((theme) => ({
+  chat: {
+    flex: 7,
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
     },
-    border : "1px solid black"
-}
-}))
+    border: "1px solid black",
+    display: "flex",
+    flexDirection: "column",
+  },
+  avatar : {
+    backgroundColor: red[500],
+  },
+  chat__header: {
+
+  },
+  chat__body: {
+      flex : 1,
+      backgroundImage : `url(${image})`,
+      backgroundRepeat : "repeat",
+      backgroundPosition : 'center',
+      overflow: "scroll",
+      padding : "1rem",
+      overflowX : "hidden"
+      
+    //   height : "50vh"
+  },
+  chat__footer: {
+
+    display : "flex",
+    justifyContent : "space-between",
+    alignItems : "center",
+    height : "3rem",
+    padding: "0 1rem"
+
+  },
+  footerIcons:{
+      color : "gray"
+  },
+  chat__footer__messageform : {
+flex : 1,
+// backgroundColor : "green",
+display : "flex"
+
+  },
+  messageInput:{
+    flex : 1,
+    margin : "0 2rem",
+    height :"2.7rem",
+    borderRadius : "50px",
+    border : "none",
+    "&:focus": {
+        outlineWidth : "0rem"
+       },
+    padding : "0 1rem"
+  },
+  chat__message : {
+      backgroundColor : "white",
+      position : 'relative',
+      fontSize : "1rem",
+      padding : '1rem',
+      borderRadius : '1rem',
+      width : "fit-content",
+      marginBottom : "1rem"
+      
+
+  },
+  chat__user:{
+      position : "absolute",
+      fontSize : "xx-small",
+      fontWeight : 800,
+      top : 0,
+      left : 8
+  },
+  chat__timestamp : {
+      marginLeft : "0.3rem",
+      fontSize : "xx-small"
+  },
+  chat__receiver : {
+    backgroundColor : "white",
+    position : 'relative',
+    fontSize : "1rem",
+    padding : '1rem',
+    borderRadius : '1rem',
+    width : "fit-content",
+    marginBottom : "1rem",
+      marginLeft : "auto",
+      backgroundColor : "rgb(219,246,198)"
+  },
+  submitButton:{
+      display : "none"
+  }
+}));
 
 export default useStyles;
