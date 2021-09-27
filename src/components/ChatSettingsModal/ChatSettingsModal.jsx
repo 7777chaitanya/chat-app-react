ded89a6e5fc3492968dd7a55d5703cb2588121f8
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-27 13:25:01 +0530

    Added : hide add member from private chats

--- src/components/ChatSettingsModal/ChatSettingsModal.jsx
+++ src/components/ChatSettingsModal/ChatSettingsModal.jsx
@@ -1,17 +1,22 @@
-import React,{useContext} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import { ChatSettingsModalContext } from '../../contexts/ChatSettingsModalContext';
-import { ListItem, ListItemIcon, ListItemText, Divider } from '@material-ui/core';
-import InboxIcon from '@material-ui/icons/Inbox';
-import List from '@material-ui/core/List';
-import { MemberSearchModalContext } from '../../contexts/MemberSearchModalContext';
-
-
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { ChatSettingsModalContext } from "../../contexts/ChatSettingsModalContext";
+import {
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+} from "@material-ui/core";
+import InboxIcon from "@material-ui/icons/Inbox";
+import List from "@material-ui/core/List";
+import { MemberSearchModalContext } from "../../contexts/MemberSearchModalContext";
+import { AllRoomsWithDocIdContext } from "../../contexts/AllRoomsWithDocIdContext";
+import { CurrentRoomContext } from "../../contexts/CurrentRoomContext";
 
 function getModalStyle() {
   const top = 35;
-  const left = 50 ;
+  const left = 50;
 
   return {
     top: `${top}%`,
@@ -22,31 +27,47 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'relative',
+    position: "relative",
     width: "12rem",
     backgroundColor: theme.palette.background.paper,
-    borderRadius : "10px"
+    borderRadius: "10px",
   },
 }));
 
 export default function SimpleModal() {
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
-  const {open, handleOpen, handleClose} = useContext(ChatSettingsModalContext);
-  const {openMemberSearch,handleOpenMemberSearch,handleCloseMemberSearch} = useContext(MemberSearchModalContext);
+  const { open, handleOpen, handleClose } = useContext(
+    ChatSettingsModalContext
+  );
+  const { openMemberSearch, handleOpenMemberSearch, handleCloseMemberSearch } =
+    useContext(MemberSearchModalContext);
+  const { rooms, setRooms } = useContext(AllRoomsWithDocIdContext);
+  const { currentRoom, setCurrentRoom } = useContext(CurrentRoomContext);
 
+  const showAddMemberOption = () => {
+    if(currentRoom && rooms){
+    let roomFinder = rooms.find(room => room.id === currentRoom);
+
+      console.log("roomFinder=>",roomFinder) ;
+    return roomFinder.data.privateChat;
+    }
+    
+  };
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
-     <List component="nav" aria-label="main mailbox folders">
-        <ListItem button onClick={handleOpenMemberSearch}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Add member" />
-        </ListItem>
+      <List component="nav" aria-label="main mailbox folders">
+        {!showAddMemberOption() && (
+          <ListItem button onClick={handleOpenMemberSearch}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Add member" />
+          </ListItem>
+        )}
 
-        <ListItem button >
+        <ListItem button>
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -60,6 +81,7 @@ export default function SimpleModal() {
         </ListItem>
       </List>
       <Divider />
+      {console.log("roomsroomsrooms, ", rooms)}
     </div>
   );
 
