40fd827154d58d739128ee1bb3a173c678484fb7
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-27 15:49:45 +0530

    Modified : styles

--- src/appStyles.js
+++ src/appStyles.js
@@ -9,8 +9,8 @@ const  useStyles = makeStyles({
 
     },
     app__body : {
-        height : "90vh",
-        width : "90vw",
+        height : "100vh",
+        width : "100vw",
         display : "flex",
         backgroundColor : "rgb(235,235,235)",
         boxShadow : "3px 3px 2px 2px rgba(0, 0, 0, 0.2)",
